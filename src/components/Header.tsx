16c15196af12083dd26179a759c973b057c4fbee
vishal dev <vishalrajputt88@gmail.com>
2025-06-23 19:26:41 +0530

    header design proper

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -76,6 +76,8 @@ const Header = () => {
             <Link to="/volunteer" className="hover:text-[#6a1b9a]">Volunteer</Link>
             <Link to="/blogs" className="hover:text-[#6a1b9a]">Blogs</Link>
             <Link to="/contact" className="hover:text-[#6a1b9a]">Contact</Link>
+            <Link to="/login" className="hover:text-[#6a1b9a]"> Login </Link>
+
           </div>
 
           {/* Desktop Right Section: Location and Login */}
@@ -121,10 +123,10 @@ const Header = () => {
               </>
             ) : (
               <div className="flex items-center space-x-2">
-                <Link to="/login" className="bg-[#fff] border-[#F1A501] border-1 text-[#000] px-5 py-2 rounded-[40px] font-semibold text-base">
+                {/* <Link to="/login" className="bg-[#fff] border-[#F1A501] border-1 text-[#000] px-5 py-2 rounded-[40px] font-semibold text-base">
                   Login
-                </Link>
-                <Link to="/list-your-ground" className="ml-2 bg-[#F1A501] text-white px-5 py-2 rounded-[40px] font-semibold text-base hover:bg-[#d18e00] transition-colors">
+                </Link> */}
+                <Link to="/list-your-ground" className="bg-[#fff] border-[#F1A501] border-1 text-[#000] px-5 py-2 rounded-[40px] font-semibold text-base no-underline">
                   List Your Ground
                 </Link>
               </div>
